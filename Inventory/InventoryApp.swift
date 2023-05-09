992feb3a0f6d92d149bc585ae372e2bd80e0b3d4
Sam Askling <samaskling@gmail.com>
2023-05-09 17:08:27 -0500

    Initial Commit

---
+++ Inventory/InventoryApp.swift
@@ -0,0 +1,17 @@
+//
+//  InventoryApp.swift
+//  Inventory
+//
+//  Created by Smay on 5/9/23.
+//
+
+import SwiftUI
+
+@main
+struct InventoryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
