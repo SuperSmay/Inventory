abad47b240f3136577c278226338eb6734b4a895
Sam Askling <samaskling@gmail.com>
2023-12-30 12:38:17 -0600

    Commiting in case I want this code later

--- Inventory/HouseListView.swift
+++ Inventory/HouseListView.swift
@@ -17,13 +17,33 @@ struct HouseListView: View {
     
     var body: some View {
         NavigationStack(path: $houseDisplayStack) {
-            Form {
-                ForEach(houses) { house in
-                    NavigationLink(value: house) {
-                        Text(house.name)
+            // TODO: change number of items in row dynamically
+            let rowLength = 2
+            let gridItemWidth = 175
+            Grid(alignment: .top) {
+                
+                let rowRange = Array(0..<houses.count/rowLength+1)
+                
+                ForEach(rowRange, id: \.self) { rowIndex in
+                    
+                    let columnRange = Array(0..<rowLength)
+                    
+                    GridRow {
+                        
+                        ForEach(columnRange, id: \.self) { columnIndex in
+                            
+                            let indexInList = rowIndex * rowLength + columnIndex
+                            
+                            if indexInList < houses.count {
+                                
+                                let house = houses[indexInList]
+                                
+                                NavigationLink(value: house) {
+                                    HouseGridItemView(house: house, gridItemWidth: CGFloat(gridItemWidth))
+                                }
+                            }
+                        }
                     }
-                    
-                    
                 }
             }
             .toolbar {
@@ -43,7 +63,29 @@ struct HouseListView: View {
                 HouseView(house: house)
             }
             .navigationTitle("Inventory")
+            
+            Spacer()
         }
+    }
+}
+
+struct HouseGridItemView: View {
+    
+    let house: House
+    let gridItemWidth: CGFloat
+    
+    var body: some View {
+        Text(house.name)
+            .font(.title2)
+            .bold()
+            .padding()
+            .multilineTextAlignment(/*@START_MENU_TOKEN@*/.leading/*@END_MENU_TOKEN@*/)
+            .frame(width: gridItemWidth, height: 100)
+            .background {
+                Rectangle()
+                    .foregroundStyle(.secondary)
+            }
+            .clipShape(RoundedRectangle(cornerRadius: 20, style: /*@START_MENU_TOKEN@*/.continuous/*@END_MENU_TOKEN@*/))
     }
 }
 
@@ -54,8 +96,10 @@ struct HouseListView: View {
         let container = try ModelContainer(for: House.self, configurations: config)
         
         let example = House(name: "Home Sweet Home", address: "142 Fellowship Drive", storedItems: [])
+        let example2 = House(name: "Home Sweet Home 2", address: "142 Fellowship Drive", storedItems: [])
+        let example3 = House(name: "Home Sweet Home 3", address: "142 Fellowship Drive", storedItems: [])
         
-        return HouseListView(houses: [example])
+        return HouseListView(houses: [example, example2, example3, example3, example3, example3])
             .modelContainer(container)
         
     } catch {
